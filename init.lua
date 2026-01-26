97fe6e0d0765f1d5cf0221adac4778d29acca3d6
Shiku Shi <cyber.shikushi@proton.me>
2026-01-26 17:09:40 +0300

    Add json formatting

--- init.lua
+++ init.lua
@@ -711,6 +711,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
